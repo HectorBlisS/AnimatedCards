8af58b03eefb163f61669d3f3558650593c03293
Fixter <fixter@MacBook-Pro-de-Fixter.local>
2020-01-21 11:29:51 -0600

    animated cards build

---
+++ prod/precache-manifest.324b850e8073ad3d2f571a52298f3b0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bef54fbfbb00460e67bb75d62e4ff4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "518d7ad4d31daebc6524",
+    "url": "/static/css/main.f55be09c.chunk.css"
+  },
+  {
+    "revision": "794ffb24b5f82a2dd936",
+    "url": "/static/js/2.29923622.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.29923622.chunk.js.LICENSE"
+  },
+  {
+    "revision": "518d7ad4d31daebc6524",
+    "url": "/static/js/main.aaab48b3.chunk.js"
+  },
+  {
+    "revision": "bac8c547b39117d85dce",
+    "url": "/static/js/runtime-main.6aef009a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
